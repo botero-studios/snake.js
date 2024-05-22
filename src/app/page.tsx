9694cb9c3dd923c31fb0fe456d99b86945768972
Wilmar Stiven Valencia Cardona <wilmar2545@gmail.com>
2024-05-22 09:43:31 -0500

    Merge 'v0.1' into '21-snake-scenario'

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,21 @@
-import HomePage from "@/pages/HomePage";
+'use client'
+
+import { Canvas } from "@react-three/fiber";
+import { Physics } from "@react-three/rapier";
+import { Suspense, useMemo } from "react";
+import { Experience } from "./components/ThreeScene";
 
 export default function Home() {
-	return <HomePage />;
+    return (
+        <div className="w-full h-screen">
+            <Canvas shadows camera={{ position: [0, 6, 14], fov: 70 }}>
+                <color attach="background" args={["#dbecfb"]} />
+                <Suspense>
+                    <Physics debug>
+                        <Experience />
+                    </Physics>
+                </Suspense>
+            </Canvas>
+        </div>
+    );
 }
